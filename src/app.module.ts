d05896eaa5dc86e20c78ec967f46e0a459b61d56
mmelvin0581 <mmelvin0581@gmail.com>
2019-09-27 11:19:07 -0400

    connect mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,31 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 import { ProductsController } from './products/products.controller';
 import { ProductsService } from './products/products.service';
 import { ProductsModule } from './products/products.module';
 
+import { ConfigService } from './config/config.service';
+import { ConfigModule } from './config/config.module';
+
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get('MONGO_URI'),
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule,
+    ProductsModule,
+  ],
   controllers: [AppController, ProductsController],
-  providers: [AppService, ProductsService],
+  providers: [AppService, ConfigService, ProductsService],
 })
 export class AppModule {}
